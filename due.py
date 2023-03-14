76134558ce366a130cf00f70d971c505227d5439
SimoneFinessi <finessi.simone@iisgalvanimi.edu.it>
2023-03-14 10:27:40 +0000

    fatto2

---
+++ due.py
@@ -0,0 +1,10 @@
+from flask import Flask,render_template
+app = Flask(__name__)
+
+@app.route('/', methods=['GET'])
+def tour():
+    return render_template("tour2.html")
+
+if __name__ == '__main__':
+  app.run(host='0.0.0.0', port=3245, debug=True)
+  
